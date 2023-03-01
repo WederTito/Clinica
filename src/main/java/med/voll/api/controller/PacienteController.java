5d57bf400669bebeb13ff49d0311723374484a51
weder.tito <wedertito8@gmail.com>
2023-03-01 16:12:48 -0300

    create table patient and persistence in database

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,28 @@
+package med.voll.api.controller;
+
+
+import jakarta.validation.Valid;
+import med.voll.api.Paciente.DadosCadastroPaciente;
+import med.voll.api.Paciente.Paciente;
+import med.voll.api.Paciente.PacienteRepository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
+    }
+}
+
